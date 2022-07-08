2a55a72d7372c57d38ad87ef9b3f8edfa6271630
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-07-08 11:43:20 +0430

    commit

--- Routes/Auth.routes.js
+++ Routes/Auth.routes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const createError = require("http-errors");
 const User = require("../Models/User.model");
 const { authSchema } = require("../helpers/validation_schema");
-const { signAccessToken } = require("../helpers/jwt_helper");
+const {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require("../helpers/jwt_helper");
 
 router.post("/register", async (req, res, next) => {
   try {
@@ -18,8 +22,9 @@ router.post("/register", async (req, res, next) => {
     const user = new User(result);
     const savedUser = await user.save();
     const accessToken = await signAccessToken(savedUser.id);
+    const refreshToken = await signRefreshToken(savedUser.id);
 
-    res.send({ accessToken });
+    res.send({ accessToken, refreshToken });
   } catch (error) {
     if (error.isJoi === true) error.status = 422;
     next(error);
@@ -37,8 +42,9 @@ router.post("/login", async (req, res, next) => {
     if (!isMatch) throw createError.Unauthorized("Username/Password not valid");
 
     const accessToken = await signAccessToken(user.id);
+    const refreshToken = await signRefreshToken(user.id);
 
-    res.send({ accessToken });
+    res.send({ accessToken, refreshToken });
   } catch (error) {
     if (error.isJoi === true)
       return next(createError.BadRequest("Invalid Username/Password"));
@@ -47,7 +53,17 @@ router.post("/login", async (req, res, next) => {
 });
 
 router.post("/refresh-token", async (req, res, next) => {
-  res.send("Refresh token route");
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) throw createError.BadRequest();
+    const userId = await verifyRefreshToken(refreshToken);
+
+    const accessToken = await signAccessToken(userId);
+    const refToken = await signRefreshToken(userId);
+    res.send({ accessToken: accessToken, refreshToken: refToken });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/logout", async (req, res, next) => {
--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -28,14 +28,51 @@ module.exports = {
     const token = bearerToken[1];
     JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
       if (err) {
-        if (err.name === "") {
-          return next(createError.Unauthorized());
-        } else {
-          return next(createError.Unauthorized(err.message));
-        }
+        // if (err.name === "JsonWebTokenError") {
+        //   return next(createError.Unauthorized());
+        // } else {
+        //   return next(createError.Unauthorized(err.message));
+        // }
+        const message =
+          err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
+        return next(createError.Unauthorized(message));
       }
       req.payload = payload;
       next();
+    });
+  },
+
+  signRefreshToken: (userId) => {
+    return new Promise((resolve, reject) => {
+      const payload = {};
+      const secret = process.env.REFRESH_TOKEN_SECRET;
+      const options = {
+        expiresIn: "1y",
+        issuer: "mohammadhossein.com",
+        audience: userId,
+      };
+      JWT.sign(payload, secret, options, (err, token) => {
+        if (err) {
+          console.log(err.message);
+          // reject(err);
+          reject(createError.InternalServerError());
+        }
+        resolve(token);
+      });
+    });
+  },
+  verifyRefreshToken: (refreshToken) => {
+    return new Promise((resolve, reject) => {
+      JWT.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, payload) => {
+          if (err) return reject(createError.Unauthorized());
+          const userId = payload.aud
+
+          resolve(userId)
+        }
+      );
     });
   },
 };
